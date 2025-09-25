738800cecbef31a3811ef918ef8d0a334b5239f0
yanjxv <2675234552@qq.com>
2025-09-25 21:25:46 +0800

    card样式

--- src/components/menu/CardItem.vue
+++ src/components/menu/CardItem.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="card-layout">
+  <div class="card-layout" :class="classObj">
     <div class="circle-icon">
-      <img src="@/assets/svg/ic_plan.svg" alt=""></img>
+      <img :src="imgsrc" alt=""></img>
     </div>
-    <span class="text">{{ text }}</span>
+    <span class="text">{{ showText }}</span>
     <span class="count">{{ count }}</span>
 
 
@@ -11,27 +11,82 @@
 </template>
 
 <script>
+export const TYPE_TODAY = 1
+export const TYPE_TODO = 2
+export const TYPE_ALL = 3
 // 只有通过 export default 导出的组件，才能被其他文件通过 import 引入使用
 export default {
+  //用于接收父组件传递的参数
+  props: {
+    cardType: {
+      type: Number,
+      default: TYPE_TODAY,
+    },
+    isSelected: {
+      type: Boolean,
+      default: false,
+    },
+    count: {
+      type: Number,
+      default: 0,
+    }
+  },
   name: 'CardItem',
   // 数据定义部分
   data() {
     return {
       type: 1,
-      count: 2,
-      text: '今天',
     }
   },
+  computed: {
+    imgsrc() {
+      let iconName
+      switch (this.cardType) {
+        case TYPE_TODAY:
+          iconName = 'ic_today'
+          break
+        case TYPE_TODO:
+          iconName = 'ic_plan'
+          break
+        case TYPE_ALL:
+          iconName = 'ic_all'
+          break
+      }
+      // return `@/assets/svg/${iconName + (this.isSelected ? '_selected' : '')}.svg`@只在import导入时可以用
+      return `src/assets/svg/${ iconName + (this.isSelected ? '_selected' : '') }.svg`
+    },
+    showText() {
+      switch (this.cardType) {
+        case TYPE_TODAY:
+          return '今天'
+        case TYPE_TODO:
+          return '计划'
+        case TYPE_ALL:
+          return '全部'
+      }
+    },
+// 用于动态生成CSS类名对象。
+    classObj() {
+      return {
+        selected: this.isSelected,           // 根据isSelected prop决定是否添加'selected'类
+        unselected: !this.isSelected,        // 根据isSelected prop决定是否添加'unselected'类
+        today: this.cardType === TYPE_TODAY, // 根据cardType prop决定是否添加'today'类
+        plan: this.cardType === TYPE_TODO,   // 根据cardType prop决定是否添加'plan'类
+        all: this.cardType === TYPE_ALL,     // 根据cardType prop决定是否添加'all'类
+      }
+    }
+
+  },
 }
+  
 </script>
 <style scoped lang="scss">
 .card-layout { 
   padding: 8px;
   border-radius: 12px;
-  // width: 80%;
-  // margin-top: 8px;
   background: #cbcbcb;
   position: relative;
+  color: #4c4c4c;
 
   .circle-icon {
     display: flex;
@@ -40,25 +95,58 @@ export default {
     border-radius: 50%;
     width: 24px;
     height: 24px;
-    background-color: red;
   }
   img{
     width: 16px;
     height: 16px;
   }
+  .circle-icon.today {
+    background-color: var(--todo-blue);
+  }
   .count{
     font-weight: bold;
-    color: black;
+    color: inherit;
     font-size: 16px;
-    right: 8px;
+    right: 12px;
     position: absolute;
     top: 8px;
   }
   .text{
     font-size: 14px;
-    color: #4c4c4c;
+    margin-top: 2px;
+    color: inherit;
     font-weight: normal;
   }
 }
+.card-layout.selected {
+  color: white;
+  .circle-icon {
+    background-color: white;
+  }
+}
+//图片
+.card-layout.today.unselected .circle-icon {
+  background-color: var(--todo-blue);
+}
+.card-layout.plan.unselected .circle-icon {
+  background-color: var(--todo-red);
+}
+.card-layout.all.unselected .circle-icon {
+  background-color: var(--todo-gray);
+}
+//文字
+.card-layout.selected.today {
+  background-color: var(--todo-blue);
+}
 
+.card-layout.selected.plan {
+  background-color: var(--todo-red);
+}
+
+.card-layout.selected.all {
+  background-color: var(--todo-gray);
+}
+.card-layout:hover {
+  background-color: #b7b7b7;
+}
 </style>
--- src/components/menu/MenuLayout.vue
+++ src/components/menu/MenuLayout.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
     <input type="text" name="" id="" placeholder="搜索" />
-    <card-item style="width: 200px" />
+    <!-- 添加类型和点击事件 -->
+    <div class="todo-card-layout">
+      <card-item
+        class="todo-item item-left"
+        :card-type="1"
+        :is-selected="currentCard === 1"
+        @click="onClickCard(1)"
+      />
+      <card-item
+        class="todo-item"
+        :card-type="2"
+        :is-selected="currentCard === 2"
+        @click="onClickCard(2)"
+      />
+    </div>
+    <card-item
+      class="all-card"
+      :card-type="3"
+      :is-selected="currentCard === 3"
+      @click="onClickCard(3)"
+    />
   </div>
 </template>
 <script>
@@ -12,13 +32,37 @@ export default {
   components: {
     CardItem,
   },
+  data() {
+    return {
+      currentCard: 1,
+    }
+  },
+  methods: {
+    onClickCard(type) {
+      this.currentCard = type
+    },
+  },
 }
 </script>
-<style scoped>
+<style scoped lang="scss">
 input {
   width: 100%;
   height: 30px;
-  /* 设置为块级元素确保输入框下方的 CardItem 组件会换行显示在下一行 */
+  // 设置为块级元素确保输入框下方的 CardItem 组件会换行显示在下一行
   display: block;
+}
+.todo-card-layout {
+  display: flex;
+  margin-top: 8px;
+  .todo-item {
+    flex: 1;
+  }
+  .item-left {
+    margin-right: 8px;
+  }
+}
+.all-card {
+  margin-top: 8px;
+  width: 100%;
 }
 </style>
